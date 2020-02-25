6839759e1dbb167fa4a164873484c77484a08fc9
Mitzi Estrada <m_estrada17@live.com>
2020-02-25 05:12:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog(dog_name)
-  def name= dog_name
+  def .name= dog_name
     @this_dogs_name = dog_name
 end
 
-def name
+def .name
   @this_dogs_name
 end
 
-def bark
+def .bark
   puts "woof!"
 end
 
